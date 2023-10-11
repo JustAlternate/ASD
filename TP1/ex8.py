27eb928a2dfe72eb23e50f4974ba8c07454fd014
JustAlternate <lololollololol890@gmail.com>
2023-10-11 15:31:44 +0200

    rere

--- TP1/ex8.py
+++ TP1/ex8.py
@@ -6,22 +6,18 @@ def suite_collatz(N):
     return 1 + suite_collatz(3*N + 1)
 
 
-print(suite_collatz(13))
-
 print("Question 1")  # 6171 : 262
 
 biggest = (0, 0)
-for i in range(1, 10_000):
+for i in range(1, 10_000+1):
     total = suite_collatz(i)
-    if total > biggest[1]:
-        print(i, total)
-        biggest = i, total
+    print(i, total)
 
 print("Question 2")  # 8400511 : 686
 
 biggest = (0, 0)
-for i in range(1, 10_000_000):
+for i in range(1, 10_000_000+1):
     total = suite_collatz(i)
-    if total > biggest[1]:
+    if total >= biggest[1]:
         print(i, total)
         biggest = i, total
